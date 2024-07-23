45dc7f124661c97e574515c01be01b5420b02bf3
Gianluca Gregori <gianlucagregori@hotmail.it>
2024-07-23 09:57:49 +0200

    v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path:"/login",component:login},
-    {path:"/Gym",component:gym},
-    {path:"/User",component:utente}
 ];
