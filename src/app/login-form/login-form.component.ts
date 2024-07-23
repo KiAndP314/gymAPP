6d35b3f49281acf7ba8261ba0fd97a5ffac8d23a
Gianluca Gregori <gianlucagregori@hotmail.it>
2024-07-23 12:15:27 +0200

    v2

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
 
+  constructor(private service:LoginService){}
 
+  regForm:FormGroup = new FormGroup(
+    {
+      name : new FormControl('',[Validators.required]),
+      surname : new FormControl('',[Validators.required]),
+      dob: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required])
+    }
+  );
   
+  salva()
+  {
+    
+  }
 }
