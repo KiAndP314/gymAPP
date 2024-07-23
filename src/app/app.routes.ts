3fbf1457a8df4049a9cf9e3fb4f0c2027e48a9cd
andrea <andreapo97@outlook.it>
2024-07-23 09:04:57 +0200

    base 01 angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"/login",component:login},
+    {path:"/Gym",component:gym},
+    {path:"/User",component:utente}
+];
