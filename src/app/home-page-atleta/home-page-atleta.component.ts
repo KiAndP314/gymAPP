3ae8eef6c62bb627a12b4e77addbe2474a8369c0
andrea <andreapo97@outlook.it>
2024-07-23 15:27:04 +0200

    user service

--- src/app/home-page-atleta/home-page-atleta.component.ts
+++ src/app/home-page-atleta/home-page-atleta.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home-page-atleta',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home-page-atleta.component.html',
   styleUrl: './home-page-atleta.component.css'
 })
