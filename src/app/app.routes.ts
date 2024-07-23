1c91687e7b2b853244f95cd7fd1e86ca6a88abb4
andrea <andreapo97@outlook.it>
2024-07-23 09:26:31 +0200

    adds login, user, gym component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { GymComponent } from './gym/gym.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path:"/login",component:login},
-    {path:"/Gym",component:gym},
-    {path:"/User",component:utente}
+    {path:"/login",component:LoginComponent},
+    {path:"/Gym",component:GymComponent},
+    {path:"/User",component:UserComponent}
 ];
---
+++ src/app/model/Gym.ts
@@ -0,0 +1,7 @@
+export interface Gym
+{
+    id:number;
+    name:string;
+    address:string;
+    email:string;
+}
