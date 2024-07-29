b99404e08b5d059cca0439def17a92a60429a937
Davide Spera <speradavide2001@gmail.com>
2024-07-29 12:22:39 +0200

    V

--- src/app/home-page-atleta/home-page-atleta.component.ts
+++ src/app/home-page-atleta/home-page-atleta.component.ts
@@ -16,6 +16,18 @@ import { User } from '../model/User';
 export class HomePageAtletaComponent {
   constructor(private userService: UserService,private route: ActivatedRoute) { }
 
+    user = {
+      image: 'https://example.com/path-to-profile-image.jpg',
+      name: 'Nome',
+      surname: 'Cognome',
+      birthdate: new Date('1990-01-01'),
+      gym: 'Palestra XYZ',
+      medicalCertificate: true,
+      email: 'utente@example.com',
+      subscriptionType: 'Annuale',
+      subscriptionExpiry: new Date('2025-01-01'),
+      courses: ['Yoga', 'Pilates', 'Bodybuilding', 'Cardio']
+    };
   
 
 
